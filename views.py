65c76dfe16ce09d2d6c49f86275f3d648971d734
dipika-singhania <singhania.dipika1@gmail.com>
2015-12-20 22:57:45 +0530

    Added article details page

--- views.py
+++ views.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
@@ -11,28 +13,38 @@ from django.template import RequestContext, loader
 def index(request):
 	weekdays = ['Monday',"Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 	alreadyPublishedArticles = Article.objects.filter(publication_date__lte=datetime.date.today()).order_by('publication_date').reverse()
-	alreadyPublishedArticles.extra(order_by = ['-publication_date'])
-
 	page_size = 4
-	if len(alreadyPublishedArticles)!=0:
-		paginator = Paginator(alreadyPublishedArticles, page_size)
-		page = request.GET.get('page')
-		if(page==None):
-			page = 1
-		try:
-		   articlePerPage = paginator.page(page)
-		except PageNotAnInteger:
-			articlePerPage = paginator.page(1)
-		except EmptyPage:
-			articlePerPage = paginator.page(paginator.num_pages)
-		return render(request, 'Articles/ArticleList.html', 
-		{'alreadyPublishedArticles':alreadyPublishedArticles , 'articlePerPage': articlePerPage}
-		)
-	else:
-		return HttpResponse(":-\( No Articles at all ")
+	paginator = Paginator(alreadyPublishedArticles, page_size)
+	page = request.GET.get('page')
+	if(page==None):
+		page = 1
+	try:
+	   articlePerPage = paginator.page(page)
+	except PageNotAnInteger:
+		articlePerPage = paginator.page(1)
+	except EmptyPage:
+		articlePerPage = paginator.page(paginator.num_pages)
+	return render(request, 'Articles/ArticleList.html', 
+	{'alreadyPublishedArticles':alreadyPublishedArticles , 'articlePerPage': articlePerPage}
+	)
+	
 	
 def detail(request, article_id):
-	return HttpResponse("hello dipika nice job , keep going %" , article_id)
+	alreadyPublishedArticles = Article.objects.filter(publication_date__lte=datetime.date.today()).order_by('publication_date').reverse()
+	page_size = 4
+	paginator = Paginator(alreadyPublishedArticles, page_size)
+	page = request.GET.get('page')
+	if(page==None):
+		page = 1
+	try:
+	   articlePerPage = paginator.page(page)
+	except PageNotAnInteger:
+		articlePerPage = paginator.page(1)
+	except EmptyPage:
+		articlePerPage = paginator.page(paginator.num_pages)
+	article_detail = Article.objects.get(id=article_id)
+	return render(request, 'Articles/ArticleDetail.html' , {'article_detail':article_detail,'articlePerPage': articlePerPage})
+
 	
 def random_generator(request):
 	alreadyPublishedArticles = Article.objects.filter(publication_date__lte=datetime.date.today())
